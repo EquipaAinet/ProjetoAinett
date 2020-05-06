50394bb394b5bd4848081b2778fa1d146cee2e6a
DiogoFran <51442304+DiogoFran@users.noreply.github.com>
2020-05-06 16:43:22 +0100

    movimentos

--- resources/views/movimentos/index.blade.php
+++ resources/views/movimentos/index.blade.php
@@ -1,22 +1,24 @@
 @extends('layout')
 @section('content')
 
-<h2>movimentos</h2>
-<h2>{{$movimentos}}</h2>
+<h2>Movimentos</h2>
+
 <table>
     <tr>
-        
         <th>Data</th>
-        <th>Saldo inicial</th>
-        
-        
+        <th>Valor</th>
+        <th>Saldo Inicial</th>
+        <th>Saldo Final</th>
+        <th>Tipo</th>
+
     </tr>
         @foreach ($movimentos as $mov)
             <tr>
-            
-                
                 <td>{{$mov->data}}</td>
+                <td>{{$mov->valor}}€</td>
                 <td>{{$mov->saldo_inicial}}€</td>
+                <td>{{$mov->saldo_final}}€</td>
+                <td>{{$mov->tipo}}</td>
             </tr>
         @endforeach
 </table>
