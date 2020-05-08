c27231da9a816cdc3fddd5dc5302043228505f49
DiogoFran <51442304+DiogoFran@users.noreply.github.com>
2020-05-08 13:31:08 +0100

    register

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable implements MustVerifyEmail
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password', 'adm', 'bloqueado',
+        'name', 'email', 'password', 'adm', 'bloqueado','NIF','telefone','foto'
     ];
 
     /**
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -61,6 +61,21 @@
                             </div>
                         </div>
 
+                        <div class="form-group row">
+                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('NIF') }}</label>
+
+                        <div class="col-md-6">
+                                <input id="NIF" type="text" class="form-control @error('NIF') is-invalid @enderror" name="NIF" value="{{ old('NIF')}}"  autofocus>
+
+                                @error('NIF')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+                      
+
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
@@ -68,6 +83,7 @@
                                 </button>
                             </div>
                         </div>
+                        
                     </form>
                 </div>
             </div>
